22894a82a51768ba865e9bdba93bc607f29d01bf
Kyle Bue <kylebue9@gmail.com>
2017-08-29 00:09:22 -0700

    Added Interaction Functionality
    -Added new Interactables script that is a base class for for any interactables in the game
    -Added functionality for player to "interact" with any interactable object

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,16 +6,59 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
+    private Transform target;
     private NavMeshAgent agent;
-
+    
 	// Use this for initialization
 	void Start ()
     {
         agent = GetComponent<NavMeshAgent>();
 	}
+
+    void Update ()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
 	
     public void MoveToPoint (Vector3 point)
     {
         agent.SetDestination(point);
     }
+
+    public void FollowTarget (Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius * 0.8f;
+        agent.updateRotation = false;
+
+        target = newTarget.interactionTransform;
+    }
+
+    public void StopFollowingTarget ()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+
+        target = null;
+    }
+
+    private void FaceTarget ()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    //public IEnumerator UpdateTargetPosition ()
+    //{
+    //    Debug.Log("MOVE");
+    //    yield return new WaitForSeconds(0.25f);
+    //    if (target != null)
+    //    {
+    //        agent.SetDestination(target.position);
+    //    }
+    //}
 }
